a9c196cb03fd294e6b9ebb4eca12a9ccc6ef4720
Saurabh Tripathi <115134438+Saurabh8840@users.noreply.github.com>
2025-06-06 23:02:46 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,10 +4,11 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true,
+       
         trim: true,
         lowercase: true,
         minLength: 3,
+         unique: true,
         maxLength: 30
     },
     password: {
